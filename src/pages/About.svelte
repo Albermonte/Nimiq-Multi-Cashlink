bd56623d9c2e76e0cb673089640e1b2c7cc18143
Alberto Monterroso <14013679+Albermonte@users.noreply.github.com>
2021-05-01 12:57:25 +0200

    Open Want to know more on new tab

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -22,8 +22,9 @@
 			5. All done, click on Create Cashlinks and proceed with the payout.
 		</span><br />
 	</p>
-	<a href="https://github.com/Albermonte/Nimiq-Multi-Cashlink#how-does-it-work"
-		>Want to know more?</a
+	<a
+		href="https://github.com/Albermonte/Nimiq-Multi-Cashlink#how-does-it-work"
+		target="_blank">Want to know more?</a
 	>
 </main>
 
