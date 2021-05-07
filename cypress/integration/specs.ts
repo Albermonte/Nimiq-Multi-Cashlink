0f04f1ae0df67c3656989b8f2c1513da72e6b3da
Alberto Monterroso <14013679+Albermonte@users.noreply.github.com>
2021-05-07 10:52:53 +0200

    Removed cypress-expected and always load wallet

--- cypress/integration/specs.ts
+++ cypress/integration/specs.ts
@@ -1,12 +1,12 @@
+const walletString =
+	"[109,61,221,91,63,138,201,125,88,21,118,170,11,220,72,61,153,192,96,152,224,209,61,166,85,94,46,26,203,156,143,105,70,5,140,126,218,146,220,121,192,29,86,223,130,97,202,210,222,29,237,66,172,51,17,139,113,255,7,15,217,241,7,246,0]";
+
 describe("Test Cashlink Creation", () => {
 	it("creates 3 cashlinks", () => {
 		cy.visit("/");
+		window.localStorage.wallet = walletString;
 		cy.get(".consensus-established", { timeout: 60000 }).should("be.visible");
 		cy.screenshot("ConsensusEstablished", { capture: "fullPage" });
-
-		const walletString =
-			"[109,61,221,91,63,138,201,125,88,21,118,170,11,220,72,61,153,192,96,152,224,209,61,166,85,94,46,26,203,156,143,105,70,5,140,126,218,146,220,121,192,29,86,223,130,97,202,210,222,29,237,66,172,51,17,139,113,255,7,15,217,241,7,246,0]";
-		window.localStorage.wallet = walletString;
 
 		cy.get(".field-amount:nth-child(1) > .nq-input").click();
 		cy.get(".field-amount:nth-child(1) > .nq-input").type("0.00001");
@@ -23,6 +23,7 @@ describe("Test Cashlink Creation", () => {
 
 	it("check created cashlinks", () => {
 		cy.visit("/success");
+		window.localStorage.wallet = walletString;
 		cy.get(".consensus-established", { timeout: 60000 }).should("be.visible");
 		cy.screenshot("CheckingCashlinks", { capture: "fullPage" });
 
