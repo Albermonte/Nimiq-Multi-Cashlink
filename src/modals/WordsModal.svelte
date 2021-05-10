bfe75cb6b3b299c5ee8495af40c9f14545e8a927
Alberto Monterroso <14013679+Albermonte@users.noreply.github.com>
2021-05-10 20:15:27 +0200

    Replaced var with const

--- src/modals/WordsModal.svelte
+++ src/modals/WordsModal.svelte
@@ -16,7 +16,7 @@
 		privateKeyMnemonic.forEach((word, index) => {
 			text += `${word}\n`;
 		});
-		var element = document.createElement("a");
+		const element = document.createElement("a");
 		element.setAttribute(
 			"href",
 			"data:text/plain;charset=utf-8," + encodeURIComponent(text),
